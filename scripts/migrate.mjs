12d985aabf00540ce26d7aab7b5b5907aa0e2f8a
Alan Tirado <atirado0617@gmail.com>
2021-06-02 18:47:27 -0700

    wip(migrate): testing pg

--- scripts/migrate.mjs
+++ scripts/migrate.mjs
@@ -2,19 +2,17 @@ import { config } from "dotenv";
 config();
 import { createDb, migrate } from "postgres-migrations";
 import path from "path";
+import { Client } from "pg";
 
-const dbConfig = {
-  user: process.env["DB_USER"],
-  password: process.env["DB_PASSWORD"],
-  host: process.env["DB_HOST"],
-  port: Number(process.env["DB_PORT"]),
-};
+const client = new Client(process.env["DATABASE_URL"]);
 
 const doMigrate = async () => {
+  console.log("Connecting to DB...");
+  await client.connect();
   console.log("Creating DB...");
   try {
     await createDb(process.env["DB_NAME"], {
-      ...dbConfig,
+      client,
     });
   } catch (e) {
     console.error(e);
@@ -22,8 +20,7 @@ const doMigrate = async () => {
   console.log("Starting migration...");
   await migrate(
     {
-      ...dbConfig,
-      database: process.env["DB_NAME"],
+      client,
     },
     path.resolve(process.cwd(), "lib/migrations")
   );
