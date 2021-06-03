670debd478a86a81011223f051ed8695436391bc
Alan Tirado <atirado0617@gmail.com>
2021-06-02 23:18:23 -0700

    fix(api): database

--- scripts/migrate.mjs
+++ scripts/migrate.mjs
@@ -7,9 +7,9 @@ import pg from "pg";
 const client = new pg.Client({
   host: process.env["DB_HOST"],
   user: process.env["DB_USER"],
+  password: process.env["DB_PASSWORD"],
   database: process.env["DB_NAME"],
   port: process.env["DB_PORT"],
-  password: process.env["DB_PASSWORD"],
   ssl: {
     rejectUnauthorized: false,
   },
