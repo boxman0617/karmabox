6c75d38eb352fc75d15e7c7128d4b45a4b7d98f0
Alan Tirado <atirado0617@gmail.com>
2021-06-02 18:54:06 -0700

    fix(migrate): fixing pg

--- scripts/migrate.mjs
+++ scripts/migrate.mjs
@@ -2,9 +2,9 @@ import { config } from "dotenv";
 config();
 import { createDb, migrate } from "postgres-migrations";
 import path from "path";
-import { Client } from "pg";
+import pg from "pg";
 
-const client = new Client(process.env["DATABASE_URL"]);
+const client = new pg.Client(process.env["DATABASE_URL"]);
 
 const doMigrate = async () => {
   console.log("Connecting to DB...");
