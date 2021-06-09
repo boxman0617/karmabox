675f580b28be6acf129b6babe79c93e50256d036
Alan Tirado <atirado0617@gmail.com>
2021-06-09 13:12:36 -0700

    fix(twitter): fixing twitter bulk call

--- twitter/index.mjs
+++ twitter/index.mjs
@@ -3,6 +3,11 @@ config();
 import Twit from "twit";
 import fetch from "node-fetch";
 
+const env = {
+  host: process.env["NEXT_PUBLIC_SDK_HOST"],
+  port: process.env["NEXT_PUBLIC_SDK_PORT"],
+};
+
 const twitterConfig = {
   consumer_key: process.env["TWITTER_API_KEY"],
   consumer_secret: process.env["TWITTER_SECRET_KEY"],
@@ -19,21 +24,18 @@ const stream = t.stream("statuses/filter", { track: ["karma ++", "karma --"] });
 const karmaMatch = /((?:u\/|@)\w+)\skarma\s(\+\+|--)/g;
 
 const postBulkActions = (matches) =>
-  fetch(
-    `http://${process.env["SDK_HOST"]}:${process.env["SDK_PORT"]}/api/v1/bulk`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        actions: matches.map(([_, username, action]) => ({
-          username,
-          action,
-        })),
-      }),
-    }
-  ).then((res) => res.json());
+  fetch(`http://${env.host}:${env.port}/api/v1/bulk`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      actions: matches.map(([_, username, action]) => ({
+        username,
+        action,
+      })),
+    }),
+  }).then((res) => res.json());
 
 const getReply = (screenName, response) =>
   `@${screenName} ${response.map(
@@ -43,25 +45,22 @@ const getReply = (screenName, response) =>
       } ${Math.abs(actionDiff)} karma! `
   )}`.trim();
 
-stream.on(
-  "tweet",
-  async ({ text, user: { screen_name: screenName } }) => {
-    const matches = [...text.matchAll(karmaMatch)];
+stream.on("tweet", async ({ text, user: { screen_name: screenName } }) => {
+  const matches = [...text.matchAll(karmaMatch)];
 
-    if (Boolean(matches.length)) {
-      console.log("Tweet", text);
-      const response = await postBulkActions(matches);
-      const reply = getReply(screenName, response);
-      t.post(
-        "statuses/update",
-        {
-          status: reply,
-        },
-        (err, { text: replyText }) => {
-          if (err) throw err;
-          console.log(replyText);
-        }
-      );
-    }
+  if (Boolean(matches.length)) {
+    console.log("Tweet", text);
+    const response = await postBulkActions(matches);
+    const reply = getReply(screenName, response);
+    t.post(
+      "statuses/update",
+      {
+        status: reply,
+      },
+      (err, { text: replyText }) => {
+        if (err) throw err;
+        console.log(replyText);
+      }
+    );
   }
-);
+});
